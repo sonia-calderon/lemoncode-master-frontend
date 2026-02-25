c5cd6010d58f090c9ed354cddeefe404cbc99b2e
sonia-calderon <80039964+sonia-calderon@users.noreply.github.com>
2026-02-25 13:59:31 +0100

    add optional exercise

--- lab-module03-bundling/vite/vite.config.ts
+++ lab-module03-bundling/vite/vite.config.ts
@@ -1,10 +1,27 @@
 import { defineConfig } from "vite";
 import checker from "vite-plugin-checker";
 import { analyzer } from "vite-bundle-analyzer";
+import viteCompression from "vite-plugin-compression";
 
 export default defineConfig({
     plugins: [
         checker({ typescript: true }),
-        analyzer()
+        analyzer(),
+        viteCompression({
+            verbose: true,       
+            disable: false,      
+            deleteOriginFile: false, 
+            threshold: 0,        
+            algorithm: "gzip",   
+            ext: ".gz"           
+        }),
+        viteCompression({
+            verbose: true,
+            disable: false,
+            deleteOriginFile: false,
+            threshold: 0,
+            algorithm: "brotliCompress", 
+            ext: ".br"                   
+        })
     ]
 })
