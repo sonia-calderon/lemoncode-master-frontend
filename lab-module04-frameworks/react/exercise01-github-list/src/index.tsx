ef7c6240d323f6a2411a57ca8eb6d731e3884684
sonia-calderon <80039964+sonia-calderon@users.noreply.github.com>
2026-04-06 12:05:49 +0200

    add initial code

---
+++ lab-module04-frameworks/react/exercise01-github-list/src/index.tsx
@@ -0,0 +1,8 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./app";
+import "./styles.css";
+
+const rootElement = document.getElementById("root") as HTMLElement;
+const root = createRoot(rootElement);
+root.render(<App />);
