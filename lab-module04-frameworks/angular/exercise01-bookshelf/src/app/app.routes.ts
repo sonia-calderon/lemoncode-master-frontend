a6c40a6b756cfef2b55209d75264e42fb29bb544
sonia-calderon <80039964+sonia-calderon@users.noreply.github.com>
2026-06-08 17:09:26 +0200

    add routes + components

--- lab-module04-frameworks/angular/exercise01-bookshelf/src/app/app.routes.ts
+++ lab-module04-frameworks/angular/exercise01-bookshelf/src/app/app.routes.ts
@@ -1,3 +1,18 @@
 import { Routes } from '@angular/router';
+import { Home } from './features/public/home/home';
+import { Login } from './features/public/login/login';
+import { About } from './features/public/about/about';
+import { Dashboard } from './features/private/dashboard/dashboard';
+import { Gallery } from './features/private/gallery/gallery';
+import { Crud } from './features/private/crud/crud';
+import { Profile } from './features/private/profile/profile';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  { path: '', component: Home },
+  { path: 'login', component: Login },
+  { path: 'about', component: About },
+  { path: 'dashboard', component: Dashboard },
+  { path: 'gallery', component: Gallery },
+  { path: 'crud', component: Crud },
+  { path: 'profile', component: Profile },
+];
---
+++ lab-module04-frameworks/angular/exercise01-bookshelf/src/app/features/private/crud/crud.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-crud',
+  imports: [],
+  templateUrl: './crud.html',
+  styleUrl: './crud.scss',
+})
+export class Crud {}
