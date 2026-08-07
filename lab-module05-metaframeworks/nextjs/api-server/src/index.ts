336df40622dd707f1def832071add6407e3005fb
sonia-calderon <80039964+sonia-calderon@users.noreply.github.com>
2026-08-07 11:23:09 +0200

    add basic structure

---
+++ lab-module05-metaframeworks/nextjs/api-server/src/index.ts
@@ -0,0 +1,28 @@
+import { Hono } from 'hono';
+import { logger } from 'hono/logger';
+import { cors } from 'hono/cors';
+import { serve } from '@hono/node-server';
+import { serveStatic } from '@hono/node-server/serve-static';
+import { houses } from './mock-data';
+
+let db = {
+  houses,
+};
+
+const app = new Hono();
+app.use('/*', serveStatic({ root: './public' }));
+
+app.use(logger());
+app.use('/api/*', cors());
+
+app.get('/api/houses', (context) => {
+  return context.json(db.houses);
+});
+
+app.get('/api/houses/:id', (context) => {
+  return context.json(db.houses.find((c) => c.id === context.req.param('id')));
+});
+
+serve({ fetch: app.fetch, port: 3001 }, (info) => {
+  console.log(`API running on ${info.port}`);
+});
---
+++ lab-module05-metaframeworks/nextjs/app/houses/page.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+const HouseListPage = () => {
+  return (
+    <>
+      <h2>House list page</h2>
+      <ul>
+        <li>House 1</li>
+        <li>House 2</li>
+      </ul>
+    </>
+  );
+};
+
+export default HouseListPage;
