ea2580b7128d7b6963c4ee2c846aec37351328c9
sonia-calderon <80039964+sonia-calderon@users.noreply.github.com>
2026-09-24 16:38:05 +0200

    fix cypress

--- lab-module06-testing/cypress.config.ts
+++ lab-module06-testing/cypress.config.ts
@@ -2,7 +2,7 @@ import { defineConfig } from 'cypress';
 
 export default defineConfig({
   e2e: {
-    baseUrl: 'http://localhost:5173/#',
+    baseUrl: 'http://127.0.0.1:5173/#',
     specPattern: 'cypress/e2e/**/*.spec.{js,jsx,ts,tsx}',
     video: true,
     screenshotOnRunFailure: true,
